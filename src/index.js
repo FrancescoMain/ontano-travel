39d153d14ea4e8264b0efa4fe4c43b33f3525f19
francescoMain <francesco.cesarano@sfida.consulting>
2024-10-05 14:33:02 +0200

    config

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "@fontsource/inter";
+import "./i18n";
+import Translate from "./pages/Translate";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Translate />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>
   </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
