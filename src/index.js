a08ca22112d4fd12ecc7d842849dacb4e2e42733
francescoMain <francesco.cesarano@sfida.consulting>
2024-11-09 14:42:41 +0100

    Switch from HashRouter to BrowserRouter for improved routing in the application

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Routes,
   Route,
   useNavigate,
