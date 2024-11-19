076c7b4f9279cfffb54ca95cd397d9a70eaf2dbb
francescoMain <cesaranofrancescomain@gmail.com>
2024-11-19 18:52:59 +0100

    Update Dashboard layout; adjust iframe height to 100% and set container styles for improved responsiveness

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 export const Dashboard = () => {
   return (
-    <div>
+    <div style={
+      {
+        width: '100%',
+        height: '92%',
+        position: 'absolute',
+        top: 60,
+        left: 0
+      }
+    }>
 
       {/* ...Dashboard content... */}
       <iframe
         width="100%"
-        height="800"
+        height="100%"
         src="https://lookerstudio.google.com/embed/reporting/d2a380de-7276-40aa-b355-6c9d003b7507/page/7LOFE"
         frameBorder="0"
         style={{ border: 0 }}
