7ceeb962e3e8dd2d127cf4faa5df8e15972fe729
fCesarano <cesaranofrancescomain@gmail.com>
2024-12-18 18:11:46 +0100

    Refactor HomePageComponent styles: change height to min-height for better responsiveness and adjust layout margin for improved alignment.

--- src/components/HomePageComponent/HomePageComponent.jsx
+++ src/components/HomePageComponent/HomePageComponent.jsx
@@ -6,7 +6,7 @@ import hero from "../../assets/hero.jpg";
 export const HomePageComponent = () => {
   return (
     <div className="home-page overflow-auto">
-      <div className="home-page-viaggio d-flex h-100 align-items-lg-center">
+      <div className="home-page-viaggio d-flex h-100 align-items-lg-center mt-2">
         <FormViaggioComponent />
       </div>
     </div>
