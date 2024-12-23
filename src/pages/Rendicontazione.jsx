e04d2ed0e17660ebb56e2507c72d6ea05985e314
fCesarano <cesaranofrancescomain@gmail.com>
2024-12-23 09:20:37 +0100

    Add Italian locale support for date pickers in Rendicontazione component; set input format to DD/MM/YYYY

--- src/pages/Rendicontazione.jsx
+++ src/pages/Rendicontazione.jsx
@@ -7,6 +7,7 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import TextField from "@mui/material/TextField";
+import { itIT } from "@mui/x-date-pickers/locales"; // Import the Italian locale for MUI Date Pickers
 
 const Rendicontazione = () => {
   const { t } = useTranslation(); // Initialize useTranslation
@@ -37,7 +38,7 @@ const Rendicontazione = () => {
       <h1>{t("Rendicontazione")}</h1>
       <p>{t("Contenuto della pagina di rendicontazione.")}</p>
       <form onSubmit={handleDownload}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="it">
           <div className="mb-3">
             <label htmlFor="fromDate" className="form-label">
               {t("From Date")}
@@ -47,6 +48,7 @@ const Rendicontazione = () => {
               label={t("From Date")}
               value={fromDate}
               onChange={(newValue) => setFromDate(newValue)}
+              inputFormat="DD/MM/YYYY" // Set the input format to DD/MM/YYYY
               renderInput={(params) => <TextField {...params} fullWidth />}
             />
           </div>
@@ -59,6 +61,7 @@ const Rendicontazione = () => {
               label={t("To Date")}
               value={toDate}
               onChange={(newValue) => setToDate(newValue)}
+              inputFormat="DD/MM/YYYY" // Set the input format to DD/MM/YYYY
               renderInput={(params) => <TextField {...params} fullWidth />}
             />
           </div>
